ca279666bba27ac1bcaaa604bf9fc60214ae299d
BelisarioPO <belisarioperezotero03@gmail.com>
2023-04-05 19:17:57 -0300

    modulo

---
+++ data/dataModule.js
@@ -0,0 +1,125 @@
+const dataModule = {
+    usuario: {
+        userEmail: "salamaleco@gmail.com",
+        userName: "xX_DrunkDriving_Xx",
+        userPassword: "BombIran",
+        userBirth: Date(01 / 01 / 0001),
+        userDni: 00000001,
+        userImage: "./public/images/userImage.jpg"
+    },
+    productos: [{
+            productName: "Pikachu",
+            productImage: "./public/images/pika.png",
+            prductDes: "Pikachu es un Pokémon de tipo eléctrico introducido en la primera generación. Es el Pokémon más conocido de la historia",
+            productDate: Date(01/12/2003),
+        },
+        {
+            productName: "Slowpoke",
+            productImage: "./public/images/slowpoke.jpg",
+            prductDes: "Slowpoke (ヤドン Yadon en japonés) es un Pokémon de tipo agua/psíquico introducido en la primera generación.",
+            productDate: Date(09/11/2001),
+        },
+        {
+            productName: "Charmander",
+            productImage: "./public/images/charmander.jpg",
+            prductDes: "Charmander (ヒトカゲ Hitokage en japonés) es un Pokémon de tipo fuego introducido en la primera generación.",
+            productDate: Date(04/04/1968),
+        },
+        {
+            productName: "Master Ball",
+            productImage: "./public/images/master ball.webp",
+            prductDes: "Es la mejor de todas, ya que gracias a su ratio de captura nunca falla. Capturará a cualquier Pokémon sin importar el nivel, los PS o problemas de estado que posea.",
+            productDate: Date(22/11/1963),
+        },
+        {
+            productName: "Ultra Ball",
+            productImage: "./public/images/ultra.webp",
+            prductDes: "Tipo de Poké Ball introducido en la primera generación. Tiene más índice de éxito que la Super Ball. Por ende, es la más eficaz en el uso general.",
+            productDate: Date(30/01/1948),
+        },
+        {
+            productName: "Bulbasaur",
+            productImage: "./public/images/bulbasaur.png",
+            prductDes: "Bulbasaur (フシギダネ Fushigidane en japonés) es un Pokémon de tipo planta/veneno introducido en la primera generación.",
+            productDate: Date(30/04/1945),
+        },
+        {
+            productName: "Eve",
+            productImage: "./public/images/eve.png",
+            prductDes: "Eevee (イーブイ Eievui en japonés) es un Pokémon de tipo normal introducido en la primera generación.",
+            productDate: Date(05/03/1953),
+        },
+        {
+            productName: "Vaporeon",
+            productImage: "./public/images/vaporeon.png",
+            prductDes: "Vaporeon (シャワーズ Showers en japonés) es un Pokémon de tipo agua introducido en la primera generación.",
+            productDate: Date(17/12/2011),
+        },
+        {
+            productName: "Blastoise",
+            productImage: "./public/images/blastoise.png",
+            prductDes: "Blastoise es un Pokémon de tipo agua introducido en la primera generación. Es la evolución de Wartortle.",
+            productDate: Date(09/09/1976),
+        },
+        {
+            productName: "Rayquaza",
+            productImage: "./public/images/rayquaza.png",
+            prductDes: "Rayquaza (レックウザ Rayquaza en japonés) es un Pokémon legendario de tipo dragón/volador introducido en la tercera generación.",
+            productDate: Date(20/09/1922),
+        },
+    ],
+    comentarios: [
+        {
+            userName:"MorganFreeman32" ,
+            userComment: "Gran producto me ayudo mucho" ,
+            userImage:,
+        },
+        {
+            userName:"PelusaX" ,
+            userComment: "Recomendado para la casa",
+            userImage:,
+        },
+        {
+            userName: "LupoElGrande",
+            userComment:"Malisimo, me revento el microndas" ,
+            userImage:,
+        },
+        {
+            userName: "TioGrandpa4500",
+            userComment:"Producto mediocre, gaste 3 masterballs y no funcionaron" ,
+            userImage:,
+        },
+        {
+            userName:"AlfajoTerrabus1" ,
+            userComment: "Excelente compania en el ambito laboral" ,
+            userImage:,
+        },
+        {
+            userName: "Laliendre12",
+            userComment: "No recomendable, deja pelos por todas partes",
+            userImage:,
+        },
+        {
+            userName: "SHOCKRESIST" ,
+            userComment: "Me hace las cuentas y paga los impuestos",
+            userImage:,
+        },
+        {
+            userName: "Elpatitofeo",
+            userComment: "Excelente producto! A la primera masterball lo atrape",
+            userImage:,
+        },
+        {
+            userName: "CrazyMonkey11" ,
+            userComment: "Abstenerse curiosos, consumen demasiada comida",
+            userImage:,
+        },
+        {
+            userName: "GuccionmyPJS",
+            userComment: "Muy bueno para los jovenes",
+            userImage:,
+        },
+    ]
+}
+
+module.exports = dataModule;
